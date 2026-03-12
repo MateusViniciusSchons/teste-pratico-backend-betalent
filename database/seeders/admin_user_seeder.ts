a02551854662dc5f69cb69da45b95530da1b98d0
Mateus Schons <mateusvschons@gmail.com>
2026-03-12 14:20:06 -0300

    feat: Adiciona seeder de user admin

---
+++ database/seeders/admin_user_seeder.ts
@@ -0,0 +1,12 @@
+import User from '#models/user'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await User.create({
+      email: 'admin@betalent.com',
+      password: "mecontrata",
+      role: 'admin',
+    })
+  }
+}
