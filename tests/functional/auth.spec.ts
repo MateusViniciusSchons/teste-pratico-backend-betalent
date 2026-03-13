ec197be71ea2470c37953c4998539e55f9c48751
Mateus Schons <mateusvschons@gmail.com>
2026-03-13 13:54:14 -0300

    feat: Adiciona teste de auth

---
+++ tests/functional/auth.spec.ts
@@ -0,0 +1,33 @@
+import { BASE_URL } from '#tests/helpers/credentials'
+import { test } from '@japa/runner'
+
+test.group('Auth', (group) => {
+  test('Login com credenciais válidas retorna token', async ({ client, assert }) => {
+    const response = await client.post(BASE_URL+'/auth/login').json({
+      email: "admin@betalent.com",
+      password: "mecontrata",
+    })
+
+    const body = response.body() as { token: string, user: object }
+
+    response.assertStatus(200)
+    assert.exists(body.token)
+  })
+
+  test("Login com senha errada retorna 401", async ({ client }) => {
+    const response = await client.post(BASE_URL+'/auth/login').json({
+      email: "admin@betalent.com",
+      password: "senhaerrada"
+    })
+
+    response.assertStatus(401)
+  })
+
+  test("Login sem email retorna 422", async ({ client }) => {
+    const response = await client.post(BASE_URL+'/auth/login').json({
+      password: "12345678"
+    })
+
+    response.assertStatus(422)
+  })
+})
