b6d2b31d1bdda7b7f99793bcec6acfbbc7b4251d
Mateus Schons <mateusvschons@gmail.com>
2026-03-13 11:27:24 -0300

    feat: Adiciona Enum de Roles

--- start/routes.ts
+++ start/routes.ts
@@ -5,6 +5,7 @@ import TransactionsController from '#controllers/transactions_controller'
 import UsersController from '#controllers/users_controller'
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
+import { RoleEnum } from '../app/enums/roles.enum.ts'
 
 const AuthController = () => import("#controllers/auth_controller")
 
@@ -22,31 +23,31 @@ router
     router
       .group(() => {
         //Users
-        router.post('users', [UsersController, 'create']).use(middleware.role(['admin', 'manager']))
-        router.get('users', [UsersController, 'index']).use(middleware.role(['admin', 'manager']))
-        router.get('users/:id', [UsersController, 'show']).use(middleware.role(['admin', 'manager']))
-        router.put('users/:id', [UsersController, 'update']).use(middleware.role(['admin', 'manager']))
-        router.delete('users/:id', [UsersController, 'delete']).use(middleware.role(['admin', 'manager']))
+        router.post('users', [UsersController, 'create']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER]))
+        router.get('users', [UsersController, 'index']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER]))
+        router.get('users/:id', [UsersController, 'show']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER]))
+        router.put('users/:id', [UsersController, 'update']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER]))
+        router.delete('users/:id', [UsersController, 'delete']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER]))
 
         //Products
-        router.post('products', [ProductsController, 'create']).use(middleware.role(['admin', 'manager', 'finance']))
-        router.get('products', [ProductsController, 'index']).use(middleware.role(['admin', 'manager', 'finance']))
-        router.get('products/:id', [ProductsController, 'show']).use(middleware.role(['admin', 'manager', 'finance']))
-        router.put('products/:id', [ProductsController, 'update']).use(middleware.role(['admin', 'manager', 'finance']))
-        router.delete('products/:id', [ProductsController, 'delete']).use(middleware.role(['admin', 'manager', 'finance']))
+        router.post('products', [ProductsController, 'create']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER, RoleEnum.FINANCE]))
+        router.get('products', [ProductsController, 'index']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER, RoleEnum.FINANCE]))
+        router.get('products/:id', [ProductsController, 'show']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER, RoleEnum.FINANCE]))
+        router.put('products/:id', [ProductsController, 'update']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER, RoleEnum.FINANCE]))
+        router.delete('products/:id', [ProductsController, 'delete']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.MANAGER, RoleEnum.FINANCE]))
 
         //Clients
-        router.get('clients', [ClientsController, 'index']).use(middleware.role(['admin', 'user']))
-        router.get('clients/:id/transactions', [ClientsController, 'showWithTransactions']).use(middleware.role(['admin', 'user']))
+        router.get('clients', [ClientsController, 'index']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
+        router.get('clients/:id/transactions', [ClientsController, 'showWithTransactions']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
 
         //Transactions
-        router.get('transactions', [TransactionsController, 'index']).use(middleware.role(['admin', 'user']))
-        router.get('transactions/:id', [TransactionsController, 'show']).use(middleware.role(['admin', 'user']))
-        router.post('transactions/:id/chargeback', [TransactionsController, 'chargeBack']).use(middleware.role(['admin', 'finance']))
+        router.get('transactions', [TransactionsController, 'index']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
+        router.get('transactions/:id', [TransactionsController, 'show']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
+        router.post('transactions/:id/chargeback', [TransactionsController, 'chargeBack']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.FINANCE]))
 
         //Gateways
-        router.get('gateways', [GatewaysController, 'index']).use(middleware.role(['admin', 'user']))
-        router.patch('gateways/:id', [GatewaysController, 'updatePatch']).use(middleware.role(['admin', 'user']))
+        router.get('gateways', [GatewaysController, 'index']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
+        router.patch('gateways/:id', [GatewaysController, 'updatePatch']).use(middleware.role([RoleEnum.ADMIN, RoleEnum.USER]))
         
       })
       .use(middleware.auth())
