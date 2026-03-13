bee804d0836cab4d7fee6605ff5957f0654e9144
Mateus Schons <mateusvschons@gmail.com>
2026-03-13 12:00:09 -0300

    feat: Usa validator de login

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,9 +1,10 @@
  import User from '#models/user'
+import { loginValidator } from '#validators/user.validator'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
     async login({request, response}: HttpContext) {
-        const { email, password } = request.only(['email', 'password'])
+        const { email, password } = await request.validateUsing(loginValidator)
 
         const user = await User.verifyCredentials(email, password)
         const token = await User.accessTokens.create(user)
